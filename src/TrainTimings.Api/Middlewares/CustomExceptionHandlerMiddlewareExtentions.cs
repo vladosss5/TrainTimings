ca715ed799b0e752a9ea5601952feef32b21201c
vladosss5 <kiporovv1@gmail.com>
2024-06-20 02:14:26 +0700

    save

--- src/TrainTimings.Api/Middlewares/CustomExceptionHandlerMiddlewareExtentions.cs
+++ src/TrainTimings.Api/Middlewares/CustomExceptionHandlerMiddlewareExtentions.cs
@@ -2,11 +2,6 @@
 
 public static class CustomExceptionHandlerMiddlewareExtentions
 {
-    /// <summary>
-    /// Метод расширения добавлябщий миддлвейр для обработки исключений.
-    /// </summary>
-    /// <param name="builder"></param>
-    /// <returns></returns>
     public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
